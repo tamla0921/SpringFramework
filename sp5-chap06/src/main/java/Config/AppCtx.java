a74de5c95c86c245382e30496b032d4533570b9a
seo <seo@DESKTOP-B4DKHD7>
2023-07-31 16:20:06 +0900

    2023-07-31

---
+++ sp5-chap06/src/main/java/Config/AppCtx.java
@@ -0,0 +1,29 @@
+package Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import spring.Client;
+import spring.Client2;
+
+@Configuration
+public class AppCtx {
+
+    @Bean
+    @Scope("prototype")
+    public Client client() {
+        Client client = new Client();
+        client.setHost("host");
+        return client;
+    }
+    
+    @Bean(destroyMethod = "close")
+    @Scope("singleton")
+    public Client2 client2() {
+        Client2 client = new Client2();
+        client.setHost("host");
+        client.connect();
+        return client;
+    }
+}
